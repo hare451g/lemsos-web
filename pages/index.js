5d9cc93ba52c0c2ab6f3e6dee75328e1d159a8e4
hendrasadewa <hendra@stockbit.com>
2020-08-07 21:48:02 +0700

    get photos from public files

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+
 import Box from '../src/shared/Box';
 import Carousel from '../src/components/Carousel';
 import MainLayout from '../src/layouts/MainLayout';
 
-export default function Home() {
+export default function Home({ carouselPhotos }) {
   return (
     <MainLayout>
       <Box>
-        <Carousel />
+        <Carousel photos={carouselPhotos} />
       </Box>
     </MainLayout>
   );
+}
+
+export async function getStaticProps(context) {
+  // get carousel photos
+  const files = fs.readdirSync(`${process.cwd()}/public/images/carousel/`);
+  const carouselPhotos = files.map(file => `/images/carousel/${file}`)
+
+  return {
+    props: {carouselPhotos}, // will be passed to the page component as props
+  }
 }
