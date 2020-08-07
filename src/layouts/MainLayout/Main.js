f4dbf9adf91b88eea3b6731bd19c9fbf0ed3fda4
hendrasadewa <hendra@stockbit.com>
2020-08-07 20:41:33 +0700

    add margin top only when not mobile

--- src/layouts/MainLayout/Main.js
+++ src/layouts/MainLayout/Main.js
@@ -1,11 +1,14 @@
 import styled, { css } from 'styled-components';
 
 const Main = styled.main(
-  ({ theme: { colors } }) => css`
+  ({ theme: { colors, screen } }) => css`
     background-color: ${colors.lightGray6};
     padding: 8px 16px;
-    margin-top: 100px;
     flex: 1;
+
+    @media only screen and (max-width: ${screen.mobile.frame}px) {
+      margin-top: 100px;
+    }
   `
 );
 
