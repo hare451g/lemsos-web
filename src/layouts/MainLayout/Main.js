7df52f196fedf9671d1d54bd7e88b8fa98c9c8fb
hendrasadewa <hendra@stockbit.com>
2020-08-08 14:47:46 +0700

    remove padding from main layout

--- src/layouts/MainLayout/Main.js
+++ src/layouts/MainLayout/Main.js
@@ -3,7 +3,6 @@ import styled, { css } from 'styled-components';
 const Main = styled.main(
   ({ theme: { colors, screen } }) => css`
     background-color: ${colors.lightGray6};
-    padding: 8px 16px;
     flex: 1;
 
     @media only screen and (min-width: ${screen.tablet.frame}px) {
