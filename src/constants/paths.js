884df5c5b267ad28a778b02507b90c08195a82f1
hendrasadewa <hendra@stockbit.com>
2020-08-08 16:00:34 +0700

    fix paths

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,6 +1,7 @@
-const PUBLIC_ROOT = process.cwd;
+const PUBLIC_ROOT = `${process.cwd()}/public`;
 const IMAGES_PATH = `${PUBLIC_ROOT}/images`;
 const CONTENTS_PATH = `${PUBLIC_ROOT}/contents`;
+const CAROUSEL_PATH = `${IMAGES_PATH}/carousel`;
 const STORAGE_PATH = `${PUBLIC_ROOT}/storage`;
 
-export { PUBLIC_ROOT, IMAGES_PATH, CONTENTS_PATH, STORAGE_PATH };
+export { PUBLIC_ROOT, IMAGES_PATH, CAROUSEL_PATH, CONTENTS_PATH, STORAGE_PATH };
