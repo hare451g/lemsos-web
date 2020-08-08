2f983ff00ed21360ae1325af5833669de8227022
hendrasadewa <hendra@stockbit.com>
2020-08-08 19:47:06 +0700

    add region base url

--- src/containers/DonationForm/services/region.js
+++ src/containers/DonationForm/services/region.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
-import { REGION_BASE_API_URL, REGION_TYPE } from '../constants';
+import { REGION_BASE_API_URL } from '../constants';
 
 async function getProvices() {
   try {
     const list = await axios.get(`${REGION_BASE_API_URL}/provinsi`);
 
-    console.log(list);
     return {
       error: null,
       list: list.data.provinsi,
