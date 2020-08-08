70bd9e814914f8ca592677db204cc0019afcaef8
hendrasadewa <hendra@stockbit.com>
2020-08-08 15:15:37 +0700

    add divider to article

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,6 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 
 import Text from '../../shared/Text';
+import Divider from '../../shared/Divider';
 
 import Content from './Content';
 import Wrapper from './Wrapper';
@@ -11,6 +12,7 @@ function Article({ title = '', markdown = 'Content is not available' }) {
       <Text as="h1" fontSize="2.25rem">
         {title}
       </Text>
+      <Divider />
       <Content>
         <ReactMarkdown source={markdown} />
       </Content>
