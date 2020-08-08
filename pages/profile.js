4f303d017751f3e9f9a7256ae090855784d0d719
hendrasadewa <hendra@stockbit.com>
2020-08-08 14:29:29 +0700

    create profile page skeleton

---
+++ pages/profile.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+
+import Article from '../src/components/Article';
+
+import MainLayout from '../src/layouts/MainLayout';
+
+export default function Profile({ article }) {
+  return (
+    <MainLayout>
+      <Article markdown={article} title="Profil" />
+    </MainLayout>
+  );
+}
+
+export async function getStaticProps(context) {
+  // get carousel photos
+  const article = fs.readFileSync(
+    `${process.cwd()}/public/contents/main/profiles/profiles.md`,
+    'utf-8'
+  );
+
+  return {
+    props: { article }, // will be passed to the page component as props
+  };
+}
