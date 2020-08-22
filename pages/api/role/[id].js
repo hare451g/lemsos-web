d3a4569606c9cc331c86f117c309325f903aec18
hendrasadewa <hendra@stockbit.com>
2020-08-22 17:12:07 +0700

    fix role api

--- pages/api/role/[id].js
+++ pages/api/role/[id].js
@@ -3,31 +3,34 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function roleById(req, res) {
-  const query = {
-    where: {
-      id: req.query,
-    },
-  };
+  const id = parseInt(req.query.id);
 
   if (req.method === 'GET') {
-    const data = await prisma.role.findOne(query);
+    const data = await prisma.role.findOne({
+      where: { id },
+    });
     res.json({
       messages: `found role by id ${data.id}`,
       data,
     });
   } else if (req.method === 'PATCH') {
     const data = await prisma.role.update({
-      where: query.where,
+      where: { id },
       data: { name: req.body.name },
     });
 
     res.json({
-      message: `updated role id: ${role.id} name to ${name}`,
+      message: `updated role id: ${data.id} name to ${req.body.name}`,
       data,
     });
   } else if (req.method === 'DELETE') {
-    const roles = await prisma.role.delete(query);
-    res.json(roles);
+    const data = await prisma.role.delete({
+      where: { id },
+    });
+    res.json({
+      message: `deleted role id: ${id}`,
+      data,
+    });
   } else {
     res.json({
       messages: 'Role By Id API',
